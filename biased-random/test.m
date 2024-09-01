e2e800e841be7da98d9efaf7e736102e57e9040a
Matteo <palamatteocp2004@gmail.com>
2024-09-01 18:43:39 +0200

    wip: testing atan, x^2 and x^3

--- biased-random/test.m
+++ biased-random/test.m
@@ -2,11 +2,18 @@ clear all;
 close all;
 clc;
 
-plottt(-pi/2+0.1, pi/2-0.1, @(x) x.^3)
-plottt(-pi/2+0.1, pi/2-0.1, @(x) x.^5)
-plottt(-pi/2+0.1, pi/2-0.1, @(x) x.^7)
+%plot_mixed(-1, 1, @(x) x.^2)
+plot_mixed(-1, 1, @(x) x.^3)
+plot_mixed(-2000, 2000, @(x) x.^3)
+%plot_mixed(-1, 1, @(x) x.^4)
+plot_mixed(-.5, .5, @(x) atan(x))
+plot_mixed(-1, 1, @(x) atan(x))
+plot_mixed(-2, 2, @(x) atan(x))
+plot_mixed(-3, 3, @(x) atan(x))
 
-function plottt(a,b,f)
+%{
+% small optimization for the domain and coodomain computation
+function plot_increasing(a,b,f)
     figure
     axis([a b f(a) f(b)])
     grid on
@@ -14,4 +21,29 @@ function plottt(a,b,f)
     
     x = linspace(a,b,100);
     plot(x, f(x), 'r')
+
+    % lines plot
+    for d = linspace(a,b,20)
+        % plot([d d],[0 f(d)], 'r');
+        yline(f(d), 'r');
+    end
+end
+%}
+
+function plot_mixed(a,b,f)
+    ma = max(f(linspace(a,b)));
+    mi = min(f(linspace(a,b)));
+    figure
+    axis([a b mi ma])
+    grid on
+    hold on
+    
+    x = linspace(a,b,100);
+    plot(x, f(x), 'r')
+
+    % lines plot
+    for d = linspace(a,b,20)
+        % plot([d d],[0 f(d)], 'r');
+        yline(f(d), 'r');
+    end
 end
